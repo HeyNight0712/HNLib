36961973db30ac8739db84ff64c008e66c2bd359
blacknight0712 <blacknight0981@gmail.com>
2024-09-17 23:13:01 +0800

    新增 ItemExample

---
+++ src/main/java/example/ItemExample.java
@@ -0,0 +1,17 @@
+package example;
+
+import Heyblock0712.hNLib.inventory.item.SkullItem;
+import org.bukkit.inventory.ItemStack;
+
+public class ItemExample {
+    private ItemExample() {}
+
+    private void getSkull() {
+
+        // EX https://minecraft-heads.com/custom-heads/head/102896-cake
+        ItemStack skullTextures = SkullItem.fromTextures("5e2c6fdb388a519856ba73760159894d31ad71a279eaf34b91ebb187c25cde51");
+
+        ItemStack skullBase64 = SkullItem.fromBase64("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNWUyYzZmZGIzODhhNTE5ODU2YmE3Mzc2MDE1OTg5NGQzMWFkNzFhMjc5ZWFmMzRiOTFlYmIxODdjMjVjZGU1MSJ9fX0=");
+
+    }
+}
